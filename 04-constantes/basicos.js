5f0e7254aab7f02faebf310d995777cc036eed73
HectorV <hectorfabiovasquez@gmail.com>
2023-11-28 07:58:42 -0500

    Added constantes section

--- 04-constantes/basicos.js
+++ 04-constantes/basicos.js
@@ -1,1 +1,17 @@
-console.log("Hi brow")
+//Constantes se usan cuando tengamos variables que no van a cambiar de valor
+
+const PI = 3.1416;
+console.log(PI);
+
+//Las constantes se deben declarar e inicializar al mismo tiempo, la siguiente declaración genera error
+//const NUEVE;
+
+let a; //let permite la declaración de una variable vacía
+
+const NUEVE = 9;
+
+a = "kenai";
+console.log(a);
+
+//No puedes reasignar valores a una constante, genera error
+//PI = 3.15;
