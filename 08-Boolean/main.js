83992bdf8433783d02291ffbde5b0a7194cd9585
HectorV <hectorfabiovasquez@gmail.com>
2023-12-01 08:00:51 -0500

    Added Boolean chapter

---
+++ 08-Boolean/main.js
@@ -0,0 +1,20 @@
+console.log("boolean chapter");
+
+let verdadero = true;
+let falso =  false;
+
+let v = Boolean(true);
+let f = Boolean(false);
+
+console.log(verdadero, falso);
+console.log(typeof verdadero, typeof falso);
+console.log(v, f);
+
+console.log(Boolean(0));
+console.log(Boolean(-7));
+console.log(Boolean(""));
+console.log(Boolean("  "));
+
+/**MDN truthy
+ * Valores que tienden a verdadero y a falso
+ */
