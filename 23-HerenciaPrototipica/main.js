cbac72d53da2959216d16fe3a7ddcb4ff2f033fa
HectorV <hectorfabiovasquez@gmail.com>
2024-01-06 08:30:09 -0500

    Added example to herencia prototipica module

--- 23-HerenciaPrototipica/main.js
+++ 23-HerenciaPrototipica/main.js
@@ -1,3 +1,46 @@
+/**
+ * En JS un prototipo es un mecanismo que permite a un objeto heredar
+ * propiedades y métodos de otro objeto. Cada objeto en JS tiene un prototipo
+ * y cuando se intenta acceder a una propiedad o metodo de un objeto, el motor de
+ * JS busca primero en el propio objeto y luego en su cadena de prototipos.
+ *
+ * Cada objeto puede tener un prototipo que es otro objeto del cual hereda propiedades
+ * y mètodos. Cuando se intenta acceder a una propiedad o método de un objeto y
+ * este no la tiene JS busca en su propio prototipo y así sucesivamente hasta llegar
+ * al objeto 'Object.prototype', que es el último eslabón de la cadena de
+ * prototipos.
+ *
+ * Ejemplo:
+ */
+
+//Función constructora (prototipo)
+function Vehiculo() {
+  this.tipo = "Automóvil";
+}
+
+//Agregando un método al prototipo de Vehiculo
+Vehiculo.prototype.acelerar = function () {
+  console.log("Acelerando... Run Run");
+};
+
+//Objeto creado mediante la función contstructora Vehiculo
+let coche = new Vehiculo();
+
+//Agregando una propiedad al objeto coche
+coche.modelo = "Sedán";
+
+//Accediendo a propiedades y métodos
+console.log(coche.modelo);
+coche.acelerar();
+console.log(coche.tipo);
+
+/**
+ * 'vehiculo' es un objeto que actúa como prototipo
+ * 'coche' es un objeto que hereda del prototipo 'vehiculo' mediante 'Object.create(vehiculo)'
+ * El objeto 'coche' tiene su propia propiedad 'modelo', pero hereda el método
+ * 'acelerar' y la propiedad 'tipo' de su prototipo 'vehiculo'
+ */
+
 //Métodos agregados al prototipo de la función constructora
 Animal.prototype.sonar = function () {
   console.log("Hago sonidos porque estoy vivo");
