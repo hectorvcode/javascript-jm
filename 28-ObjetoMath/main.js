52cc0927f58db375305847728b19c171bc4dae9e
HectorV <hectorfabiovasquez@gmail.com>
2024-01-06 21:47:59 -0500

    Added Objeto Math Module

---
+++ 28-ObjetoMath/main.js
@@ -0,0 +1,14 @@
+console.log(Math);
+console.log(Math.PI);
+
+console.log(Math.abs(-7.8));
+console.log(Math.ceil(7.8));
+console.log(Math.floor(7.8));
+console.log(Math.round(7.2));
+console.log(Math.round(7.8));
+console.log(Math.round(7.49));
+console.log(Math.sqrt(36));
+console.log(Math.pow(2, 3));
+console.log(Math.sign(-9));
+console.log(Math.random()); //Numero aleatorio entre 0 y 1
+console.log(Math.round(Math.random() * 1000));
