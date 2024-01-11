080888f080f92ea013c10ff769d9a539e6573d6e
HectorV <hectorfabiovasquez@gmail.com>
2024-01-11 07:41:12 -0500

    Added Ejercicio 02 logica de programacion

---
+++ 35-EjerciciosLogica-02/main.js
@@ -0,0 +1,47 @@
+console.log("Ejercicio 35");
+/**
+ * 5) Programa una función que invierta las palabras de una cadena de texto,
+ * pe. miFuncion("Hola Mundo") devolverá "odnuM aloH".
+ */
+
+const reverseStr = (str = "") => {
+  if (!str) return console.warn("No ingresaste un texto");
+  let newStr = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  return console.info(newStr);
+};
+
+reverseStr("Hola Mundo");
+
+/**
+ * 6) Programa una función para contar el número de veces que se repite una palabra en un texto largo,
+ * pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+ */
+
+const repeatedPattern = (str = "", pattern = undefined) => {
+  if (!str) return console.warn("No ingresaste un texto");
+  if (pattern === undefined)
+    return console.warn("No ingresaste el patrón a buscar");
+
+  let regex = new RegExp(pattern, "g");
+  let coincidences = str.match(regex);
+  if (coincidences === null) return console.warn("No hay coincidencias");
+  return console.info(coincidences ? coincidences.length : 0);
+};
+
+repeatedPattern("hola mundo, adios mundo, hi mundo, hey mundo", "mundo");
+repeatedPattern("", "mundo");
+repeatedPattern("hola mundo, adios mundo, hi mundo, hey mundo");
+repeatedPattern("hola mundo, adios mundo, hi mundo, hey mundo", "o");
+
+/**
+ * 7) Programa una función que valide si una palabra o frase dada, es un palíndromo
+ * (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
+ */
+
+/**
+ * 8) Programa una función que elimine cierto patrón de caracteres de un texto dado,
+ * pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
+ */
