ba5f7bd2ec7d18ad28e2d61d633250cec2fcfe9a
HectorV <hectorfabiovasquez@gmail.com>
2024-01-11 22:27:57 -0500

    Finished Ejercicios 02 logica de programación

--- 35-EjerciciosLogica-02/main.js
+++ 35-EjerciciosLogica-02/main.js
@@ -41,7 +41,38 @@ repeatedPattern("hola mundo, adios mundo, hi mundo, hey mundo", "o");
  * (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
  */
 
+const isPalindrome = (str) => {
+  if (!str) return console.warn("No ingresaste un texto");
+
+  let strToLow = str.toLowerCase();
+  let splittedStr = strToLow.split("");
+  let newStr = "";
+  for (let i = splittedStr.length - 1; i >= 0; i--) {
+    newStr += str[i];
+  }
+  let newStrToLow = newStr.toLowerCase();
+  if (strToLow === newStrToLow) {
+    return console.info("Es palíndrome");
+  } else {
+    return console.info("No es palíndrome");
+  }
+};
+
+isPalindrome("Salas");
+
 /**
  * 8) Programa una función que elimine cierto patrón de caracteres de un texto dado,
  * pe. miFuncion("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz") devolverá  "1, 2, 3, 4 y 5.
  */
+
+const deletePattern = (str, pattern) => {
+  if (!str) return console.warn("No ingresaste un texto");
+  if (pattern === undefined)
+    return console.warn("No ingresaste el patrón a eliminar");
+
+  let regex = new RegExp(pattern, "g");
+  let result = str.replace(regex, "");
+  return console.info(result);
+};
+
+deletePattern("xyz1, xyz2, xyz3, xyz4 y xyz5", "x");
