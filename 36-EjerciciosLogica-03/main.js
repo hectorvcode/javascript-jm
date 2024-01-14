21ad2bbc347eae9fca61fc5f7a3bd07182d66e37
HectorV <hectorfabiovasquez@gmail.com>
2024-01-13 21:04:26 -0500

    Added Ejercicios Logica 03

--- 36-EjerciciosLogica-03/main.js
+++ 36-EjerciciosLogica-03/main.js
@@ -4,13 +4,39 @@ console.log("Ejercicios 36");
  * 9. Programa una función que obtenga un numero aleatorio entre 501 y 600.
  */
 
+const randomNum = () =>
+  console.info(Math.floor(Math.random() * (600 - 501 + 1) + 501));
+randomNum();
+
 /**
  * 10. Programa una función que reciba un número y evalúe si es capicúa o no
  * (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
  */
+
+const isCapicua = (num) => {
+  if (!num) return console.warn("No digitaste el número a evaluar");
+  let stringNum = num.toString();
+  let invertedStr = stringNum.split("").reverse().join("");
+  return stringNum === invertedStr;
+};
+
+console.info(isCapicua(2002));
 
 /**
  * 11. Programa una función que calcule el factorial de un número (El factorial de un entero positivo n,
  * se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5)
  * devolverá 120
  */
+
+const factorial = (n) => {
+  if (n === 0) return console.info(1);
+  if (!n) return console.warn("No ingresaste el número a evaluar");
+  if (n < 0) return console.warn("Solo evalúa números positivos");
+  let factorial = 1;
+  for (let i = 1; i <= n; i++) {
+    factorial *= i;
+  }
+  return console.info(factorial);
+};
+
+factorial(6);
--- 37-EjerciciosLogica-04/main.js
+++ 37-EjerciciosLogica-04/main.js
@@ -1,10 +1,26 @@
-console.log("Ejercicios 36");
+console.log("Ejercicios 37");
 
 /**
  * 12. Programa una función que determine si un número es primo
  * (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7)
  * devolverá true.
  */
+
+const isPrime = (num) => {
+  if (num <= 1) return console.info(false);
+  if (!num) return console.info("Ingresa número a evaluar");
+  if (num === 2) return console.info(true);
+
+  const limit = Math.sqrt(num);
+  for (let i = 2; i <= limit; i++) {
+    if (num % i === 0) {
+      return console.info(false);
+    }
+  }
+  return console.info(true);
+};
+
+isPrime(7);
 
 /**
  * 13. Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
