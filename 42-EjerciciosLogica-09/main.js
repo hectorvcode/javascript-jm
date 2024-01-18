c905242084062890f18b9ff5d5e5e2f4e54af739
HectorV <hectorfabiovasquez@gmail.com>
2024-01-17 22:57:25 -0500

    Added ejercicios logica 09

--- 42-EjerciciosLogica-09/main.js
+++ 42-EjerciciosLogica-09/main.js
@@ -1,24 +1,67 @@
+console.log("Ejercicios Logica 09");
+
 /**
 
 27) Programa una clase llamada Pelicula.
 
 La clase recibirá un objeto al momento de instanciarse con los siguentes datos: id de la película en IMDB, titulo, director, año de estreno, país o países de origen, géneros y calificación en IMBD.
   - Todos los datos del objeto son obligatorios.
-  - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 
-     7 restantes números.
+  - Valida que el id IMDB tenga 9 caracteres, los primeros 2 sean letras y los 7 restantes números.
   - Valida que el título no rebase los 100 caracteres.
   - Valida que el director no rebase los 50 caracteres.
   - Valida que el año de estreno sea un número entero de 4 dígitos.
   - Valida que el país o paises sea introducidos en forma de arreglo.
   - Valida que los géneros sean introducidos en forma de arreglo.
-  - Valida que los géneros introducidos esten dentro de los géneros 
-     aceptados*.
+  - Valida que los géneros introducidos esten dentro de los géneros aceptados*.
   - Crea un método estático que devuelva los géneros aceptados*.
-  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser 
-    decimal de una posición.
+  - Valida que la calificación sea un número entre 0 y 10 pudiendo ser decimal de una posición.
   - Crea un método que devuelva toda la ficha técnica de la película.
   - Apartir de un arreglo con la información de 3 películas genera 3 
     instancias de la clase de forma automatizada e imprime la ficha técnica 
     de cada película.
 
  */
+
+class Pelicula {
+  constructor({ id, titulo, director, estreno, pais, generos, calificacion }) {
+    this.id = id;
+    this.titulo = titulo;
+    this.director = director;
+    this.estreno = estreno;
+    this.pais = pais;
+    this.generos = generos;
+    this.calificacion = calificacion;
+    this.validarIMDB(id);
+  }
+
+  validarCadena(propiedad, valor) {
+    if (!valor) return console.warn(`${propiedad} "${valor}" está vacío`);
+    if (typeof valor !== "string")
+      return console.error(`${propiedad} "${valor}" ingresado, NO es string`);
+
+    return true;
+  }
+
+  validarLongitudCadena(propiedad, valor, longitud) {
+    if (valor.length > longitud)
+      return console.error(
+        `${propiedad} "${valor}" excede el número de caracteres permitidos (${longitud})`
+      );
+
+    return true;
+  }
+
+  validarIMDB(id) {
+    if (this.validarCadena("IMDB id", id)) {
+      if (!/^([a-z]){2}([0-9]{7})$/.test(id)) {
+        return console.error(
+          `IMDB id "${id}" no es válido, debe tener 9 caracteres, los dos primeros letras minúsculas y los 7 restantes números`
+        );
+      }
+    }
+  }
+}
+
+const peli = new Pelicula({
+  id: "tt1234567",
+});
