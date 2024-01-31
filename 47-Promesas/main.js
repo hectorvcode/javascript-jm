e35eaf01509c452e498949d3d0c699d1cffb44d5
HectorV <hectorfabiovasquez@gmail.com>
2024-01-31 07:20:28 -0500

    Added Promises module

---
+++ 47-Promesas/main.js
@@ -0,0 +1,50 @@
+console.log("Promesas");
+
+function cuadradoCallback(value, callback) {
+  setTimeout(() => {
+    callback(value, value * value);
+  }, 0 | (Math.random() * 1000));
+}
+
+function cuadradoPromise(value) {
+  if (typeof value !== "number")
+    return Promise.reject(
+      `Error, el valor "${value}" ingresado, no es un número`
+    );
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        value: value,
+        result: value * value,
+      });
+    }, 0 | (Math.random() * 1000));
+  });
+}
+
+cuadradoPromise(0)
+  .then((obj) => {
+    console.log("Inicia promise");
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(1);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(2);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(3);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(4);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    return cuadradoPromise(5);
+  })
+  .then((obj) => {
+    console.log(`Promise: ${obj.value}, ${obj.result}`);
+    console.log("Fin Promise");
+  })
+  .catch((err) => console.error(err));
