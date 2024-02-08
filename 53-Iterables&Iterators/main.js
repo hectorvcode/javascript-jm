23bd111cac553310148b4194f96c19be71f1d2f1
HectorV <hectorfabiovasquez@gmail.com>
2024-02-08 07:13:41 -0500

    Added Iterables & Iterators module

--- 53-Iterables&Iterators/main.js
+++ 53-Iterables&Iterators/main.js
@@ -34,3 +34,36 @@ console.log(iterator.next());
  */
 
 //03:45
+
+console.log("----- Mircha -----");
+
+const iterable = [1, 2, 3, 4, 5];
+//const iterable = new Set([1, 2, 3, 3, 3, 4, 5, 6, 6]);
+//const iterable = "Hola Mundo";
+/* 
+const iterable = new Map([
+  ["nombre", "Jon"],
+  ["edad", 35],
+]); 
+*/
+
+//Accedemos al iterador del iterable
+const iterador = iterable[Symbol.iterator]();
+
+console.log(iterable);
+console.log(iterador);
+
+/*
+console.log(iterador.next());
+console.log(iterador.next());
+console.log(iterador.next());
+console.log(iterador.next());
+console.log(iterador.next());
+console.log(iterador.next());
+*/
+
+let next = iterador.next();
+while (!next.done) {
+  console.log(next.value);
+  next = iterador.next();
+}
