90dcacd4a3c8ac76359bebe35e32d4bb23fe6521
HectorV <hectorfabiovasquez@gmail.com>
2024-02-09 06:13:35 -0500

    Completed Generators module

--- 54-Generators/main.js
+++ 54-Generators/main.js
@@ -84,3 +84,20 @@ function cuadrado(val) {
     console.log({ val, resultado: val * val });
   }, Math.random() * 1000);
 }
+
+function* generador() {
+  console.log("Inicia Generador");
+  yield cuadrado(0);
+  yield cuadrado(1);
+  yield cuadrado(2);
+  yield cuadrado(3);
+  yield cuadrado(4);
+  yield cuadrado(5);
+  console.log("Termina Generador");
+}
+
+let gen = generador();
+
+for (let y of gen) {
+  console.log(y);
+}
