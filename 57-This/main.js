40cb0af1883dbf450edc5bb60f8ea28482d047d5
HectorV <hectorfabiovasquez@gmail.com>
2024-02-11 08:31:46 -0500

    Completed This module

--- 57-This/main.js
+++ 57-This/main.js
@@ -6,9 +6,55 @@ console.log("This");
  */
 
 console.log(this);
+console.log(window);
+console.log(this === window);
 
-function ejemploFuncion() {
-  console.log(this);
+this.nombre = "Contexto Global";
+console.log(this.nombre);
+
+function imprimir() {
+  console.log(this.nombre);
 }
 
-ejemploFuncion();
+imprimir();
+
+const obj = {
+  nombre: "Contexto Objeto",
+  imprimir: function () {
+    console.log(this.nombre);
+  },
+};
+
+obj.imprimir();
+
+const obj2 = {
+  nombre: "Contexto Objeto2",
+  imprimir,
+};
+
+obj2.imprimir();
+
+//Arrow function no tiene scope de bloque
+
+const obj3 = {
+  nombre: "Contexto Objeto 3",
+  imprimir: () => {
+    console.log(this.nombre);
+  },
+};
+
+obj3.imprimir();
+
+//Función constructora si crea su propio scope
+
+function Persona(nombre) {
+  this.nombre = nombre;
+  /* return function () {
+    console.log(this.nombre);
+  }; */
+  //return console.log(this.nombre);
+  return () => console.log(this.nombre);
+}
+
+let jon = new Persona("Jon");
+jon();
