6ceeba2a9fdded74b8f0796d83fd1de5ecb3ede6
HectorV <hectorfabiovasquez@gmail.com>
2024-02-24 11:41:13 -0500

    Completed Delegación de eventos module

--- 76-DelegacionEventos/main.js
+++ 76-DelegacionEventos/main.js
@@ -3,21 +3,17 @@ console.log("----- DOM: Delegación de Eventos -----");
 const $divsEventos = document.querySelectorAll(".eventos-flujo div");
 const $linkEventos = document.querySelector(".eventos-flujo a");
 
-/**
- * Con el método stopPropagation detenemos la propagación
- */
-
 function flujoEventos(e) {
   console.log(
-    `Hola te saluda ${this.className}, el click lo originó ${e.target.className}`
+    `Hola te saluda ${this}, el click lo originó ${e.target.className}`
   );
-  e.stopPropagation();
 }
 
 document.addEventListener("click", (e) => {
   console.log("click en: ", e.target);
 
   if (e.target.matches(".eventos-flujo div")) {
+    flujoEventos(e);
   }
 
   if (e.target.matches(".eventos-flujo a")) {
@@ -25,19 +21,3 @@ document.addEventListener("click", (e) => {
     e.preventDefault();
   }
 });
-
-/* $divsEventos.forEach((div) => {
-  //Evento de burbuja =  false
-  div.addEventListener("click", flujoEventos);
-});
- */
-/**
- * e.preventDefault() previene la ejecución de comportamientos por defecto
- */
-
-/* $linkEventos.addEventListener("click", (e) => {
-  alert("hola soy jon mircha");
-  e.preventDefault();
-}); */
-
-//10:12
