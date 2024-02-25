79cd33d44bd2fbe0d6be655b5e05f4ccbd107504
HectorV <hectorfabiovasquez@gmail.com>
2024-02-24 20:06:43 -0500

    Added BOM: Objetos URL, Historial y Navegador Module

---
+++ 79-BOM_Objetos_ULHistoryBrowser/main.js
@@ -0,0 +1,35 @@
+console.log("----- BOM: Objetos: URL, Historial y Navegador -----");
+
+/* console.log("*****  Objeto URL (location)   *****");
+console.log(location);
+console.log(location.origin);
+console.log(location.protocol);
+console.log(location.host);
+console.log(location.hostname);
+console.log(location.port);
+console.log(location.href);
+console.log(location.hash);
+console.log(location.search);
+console.log(location.pathname); */
+
+//location.reload();
+
+/* console.log("*****  Objeto URL (location)   *****");
+console.log(history);
+console.log(history.length);
+history.back(0);
+history.forward(13);
+history.go(3);
+history.go(-1); */
+
+console.log("*****  Objeto URL (Navigator)   *****");
+console.log(navigator);
+console.log(navigator.connection);
+console.log(navigator.geolocation);
+console.log(navigator.mediaDevices);
+console.log(navigator.mimeTypes);
+console.log(navigator.onLine);
+console.log(navigator.serviceWorker);
+console.log(navigator.storage);
+console.log(navigator.usb);
+console.log(navigator.userAgent);
