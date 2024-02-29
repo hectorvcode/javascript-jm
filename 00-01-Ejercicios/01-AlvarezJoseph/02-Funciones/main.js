84e9649a206a34d81cc1938b8e0a6082335c4664
HectorV <hectorfabiovasquez@gmail.com>
2024-02-29 05:01:35 -0500

    Added Exercise 4 - Functions

--- 00-01-Ejercicios/01-AlvarezJoseph/02-Funciones/main.js
+++ 00-01-Ejercicios/01-AlvarezJoseph/02-Funciones/main.js
@@ -43,3 +43,17 @@ function potencia(n1, n2) {
 }
 
 console.log(potencia(2, 3));
+
+/**
+ * 4. Crea una función que tome un array de números como parámetro y devuelva la suma de todos los números del array
+ */
+
+function sumaElementosArray(arrNums) {
+  let contador = 0;
+  for (let i = 0; i < arrNums.length; i++) {
+    contador = contador + arrNums[i];
+  }
+  return contador;
+}
+
+console.log(sumaElementosArray([1, 2, 3, 4, 5])); // Output: 10
