91808ff30c953480b99c2bdc92c9533fcd97a626
HectorV <hectorfabiovasquez@gmail.com>
2024-02-29 04:46:29 -0500

    Added Exercise 2 - Functions

--- 00-01-Ejercicios/01-AlvarezJoseph/02-Funciones/main.js
+++ 00-01-Ejercicios/01-AlvarezJoseph/02-Funciones/main.js
@@ -17,3 +17,19 @@ function findLargestNum(array) {
 }
 
 console.log(findLargestNum([11, 31, 333, 55, 17]));
+
+/**
+ * 2. Crea una función que tome un array de strings como parámetro y devuelva un nuevo array con todos los strings en mayúsculas
+ */
+
+console.log("Ejercicio 2");
+
+function convertToUppercase(arrayOfStrings) {
+  const newArray = [];
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    newArray.push(arrayOfStrings[i].toUpperCase());
+  }
+  return newArray;
+}
+
+console.log(convertToUppercase(["hola", "mundo", "programming"]));
